3c67ce7d8656e81f9dfcb66c49d3307ed0faf7f1
aweris <ali@akca.io>
2022-12-13 00:28:38 +0100

    feat: introduce plugins

--- pkg/sources/asdf/source.go
+++ pkg/sources/asdf/source.go
@@ -4,9 +4,12 @@
 package asdf
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	_ "embed"
 
@@ -74,4 +77,120 @@ func New(cfg config.Config, out output.Output) (*Asdf, error) {
 
 func (a *Asdf) Name() string {
 	return "asdf"
+}
+
+func (a *Asdf) GetPlugin(name string) (*types.Plugin, error) {
+	plugins, err := a.ListPlugins()
+	if err != nil {
+		return nil, err
+	}
+
+	plugin, ok := plugins[name]
+	if !ok {
+		return nil, fmt.Errorf("plugin %s not found", name)
+	}
+
+	return plugin, nil
+}
+
+func (a *Asdf) ListPlugins() (map[string]*types.Plugin, error) {
+	plugins := make(map[string]*types.Plugin)
+
+	out, err := a.execute("plugin", "list", "--urls")
+
+	// no need to check for err here, asdf returns an error if no plugins are installed
+	if out == "No plugins installed" {
+		return plugins, nil
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	plugin := bufio.NewScanner(strings.NewReader(out))
+
+	for plugin.Scan() {
+		line := strings.TrimSpace(plugin.Text())
+
+		tokens := strings.Fields(line)
+
+		if len(tokens) != 2 {
+			return nil, fmt.Errorf("unexpected output from asdf plugin list: %+v", tokens)
+		}
+
+		name := tokens[0]
+		url := tokens[1]
+
+		plugins[name] = &types.Plugin{
+			Name:   name,
+			URL:    url,
+			Source: a,
+		}
+	}
+
+	return plugins, nil
+}
+
+func (a *Asdf) ListPluginVersions(req *types.ListPluginVersionsRequest) ([]*types.PluginVersion, error) {
+	var versions []*types.PluginVersion
+
+	args := []string{"list"}
+
+	if req.AllVersions {
+		args = append(args, "all")
+	}
+
+	args = append(args, req.Name)
+
+	out, err := a.execute(args...)
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(strings.NewReader(out))
+
+	for scanner.Scan() {
+		version := strings.TrimSpace(scanner.Text())
+
+		if version == "" {
+			continue
+		}
+
+		versions = append(versions, &types.PluginVersion{Version: version})
+	}
+
+	return versions, nil
+}
+
+func (a *Asdf) InstallPluginVersion(req *types.InstallPluginVersionRequest) error {
+	plugins, err := a.ListPlugins()
+	if err != nil {
+		return err
+	}
+
+	if _, ok := plugins[req.Name]; !ok {
+		_, err := a.execute("plugin", "add", req.Name, req.URL)
+		if err != nil {
+			return fmt.Errorf("failed to install plugin %s: %w", req.Name, err)
+		}
+	}
+
+	_, err = a.execute("install", req.Name, req.Version)
+	if err != nil {
+		return fmt.Errorf("failed to install plugin %s version %s: %w", req.Name, req.Version, err)
+	}
+
+	return nil
+}
+
+func (a *Asdf) execute(args ...string) (string, error) {
+	bufOut := &bytes.Buffer{}
+	bufErr := &bytes.Buffer{}
+
+	_, err := sh.Exec(a.env, bufOut, bufErr, filepath.Join(a.path, "bin", "entrypoint.sh"), args...)
+	if err != nil {
+		return strings.TrimSuffix(bufErr.String(), "\n"), err
+	}
+
+	return strings.TrimSuffix(bufOut.String(), "\n"), err
 }
--- pkg/types/types.go
+++ pkg/types/types.go
@@ -7,4 +7,65 @@ package types
 type Source interface {
 	// Name returns the name of the source.
 	Name() string
+
+	// GetPlugin returns the plugin with the given name.
+	GetPlugin(name string) (*Plugin, error)
+
+	// ListPlugins returns a list installed plugins.
+	ListPlugins() (map[string]*Plugin, error)
+
+	// ListPluginVersions returns a list of versions of the plugin according to the given request.
+	ListPluginVersions(req *ListPluginVersionsRequest) ([]*PluginVersion, error)
+
+	// InstallPluginVersion install the given plugin with specified version.
+	InstallPluginVersion(req *InstallPluginVersionRequest) error
+}
+
+// Plugin represents a source plugin.
+type Plugin struct {
+	// Name is the name of the plugin.
+	Name string
+
+	// URL is the URL of the plugin.
+	URL string
+
+	// Source is the source of the plugin.
+	Source Source
+}
+
+// Versions returns installed versions of the plugin.
+func (p *Plugin) Versions() ([]*PluginVersion, error) {
+	return p.Source.ListPluginVersions(
+		&ListPluginVersionsRequest{
+			Name:        p.Name,
+			AllVersions: false,
+		},
+	)
+}
+
+// PluginVersion represents a version of a plugin.
+type PluginVersion struct {
+	// Version is the version of the plugin.
+	Version string
+}
+
+// ListPluginVersionsRequest is the request for listing plugin versions.
+type ListPluginVersionsRequest struct {
+	// Name of the plugin to list versions for.
+	Name string
+
+	// List all versions of the plugin.
+	AllVersions bool
+}
+
+// InstallPluginVersionRequest is the request to install a plugin with a given version.
+type InstallPluginVersionRequest struct {
+	// Name of the plugin to install.
+	Name string
+
+	// Version of the plugin to install. Depending on the source, this may be a optional.
+	Version string
+
+	// URL of the plugin to install. Depending on the source, this may be optional.
+	URL string
 }
