df44d6c352e1f4422cb39afd4d455aa9a296997f
Ali AKCA <ali@akca.io>
2022-12-12 23:51:20 +0300

    feat: use magefile sh to execute local commands (#15)

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/magefile/mage v1.14.0 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
--- pkg/sources/asdf/cmd.go
+++ pkg/sources/asdf/cmd.go
@@ -4,24 +4,11 @@
 package asdf
 
 import (
-	"fmt"
-	"io"
-	"os"
-	"os/exec"
 	"path/filepath"
+
+	"github.com/magefile/mage/sh"
 )
 
-func (a *Asdf) execute(stdout, stderr io.Writer, args ...string) error {
-	cmd := exec.Command(filepath.Join(a.path, "bin", "entrypoint.sh"), args...)
-
-	// Set process outputs
-	cmd.Stdout = stdout
-	cmd.Stderr = stderr
-
-	// prepend the env to the existing env to allow overriding from the given env
-	cmd.Env = append(os.Environ(), a.env...)
-
-	fmt.Println("asdf executing: ", cmd.String())
-
-	return cmd.Run()
+func (a *Asdf) execute(args ...string) (string, error) {
+	return sh.OutputWith(a.env, filepath.Join(a.path, "bin", "entrypoint.sh"), args...)
 }
